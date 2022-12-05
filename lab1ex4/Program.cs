27597645a16abf29031c3aab6fa32ee1b33ff17c
Lucaci <Lucaci@DESKTOP-0E3OOL2>
2022-12-05 21:22:20 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /* Scrieti un program care va recunoaste daca un numar este pozitiv negativ sau zero */
+
+            Console.WriteLine("Acest program va afisa semnul negativ sau pozitiv al unui numar");
+
+            Console.WriteLine("Introduceti numarul");
+            int x = int.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if(x < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
